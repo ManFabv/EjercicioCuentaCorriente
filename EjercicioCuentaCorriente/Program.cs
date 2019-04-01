bed3b18c23139cacde0baaf88af9368f120c4c35
Fabricio Manrique <fabricio.manrique@algacom.ch>
2019-04-01 16:40:49 -0300

    Add project files.

---
+++ EjercicioCuentaCorriente/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjercicioCuentaCorriente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
